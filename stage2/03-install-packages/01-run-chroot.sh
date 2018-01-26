ea83b23d93fc27f181633b2a2888532b80a52dae
BrunoRB <brunorb8@hotmail.com>
2018-01-26 12:10:58 -0200

    enabling raspicam by default

--- stage2/03-install-packages/01-run-chroot.sh
+++ stage2/03-install-packages/01-run-chroot.sh
@@ -1,1 +1,13 @@
+# socket io client
 pip3 install socketIO-client
+
+# enable camera
+[ -e /boot/config.txt ] || touch /boot/config.txt
+set_config_var start_x 1 /boot/config.txt
+CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
+if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+	set_config_var gpu_mem 128 /boot/config.txt
+fi
+sed /boot/config.txt -i -e "s/^startx/#startx/"
+sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
+
