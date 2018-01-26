15e0c6ec842a660a1d5b34eb3f9f2545f3753fee
BrunoRB <brunorb8@hotmail.com>
2018-01-26 18:34:50 -0200

    fixes run.sh state2 install packages

--- stage2/03-install-packages/00-run.sh
+++ stage2/03-install-packages/00-run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
-install -m 774 -g pi -o pi $BINARY_PATH ${ROOTFS_DIR}/home/pi/camera360pi
+#install -m 774 -g pi -o pi $BINARY_PATH ${ROOTFS_DIR}/home/pi/camera360pi
+install -m 774 /home/build/camera360pi ${ROOTFS_DIR}/home/pi/camera360pi
 install -m 774 files/camera360.service ${ROOTFS_DIR}/lib/systemd/system/
 
 on_chroot << EOF
 systemctl enable camera360
+chown pi.pi /home/pi/camera360pi
 EOF
