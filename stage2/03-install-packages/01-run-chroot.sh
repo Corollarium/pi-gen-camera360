65757abdce272adab734e2752f6e4cc54be35365
BrunoRB <brunorb8@hotmail.com>
2018-01-26 19:03:55 -0200

    fix camera start

--- stage2/03-install-packages/01-run-chroot.sh
+++ stage2/03-install-packages/01-run-chroot.sh
@@ -1,12 +1,8 @@
 # enable camera
 [ -e /boot/config.txt ] || touch /boot/config.txt
-set_config_var start_x 1 /boot/config.txt
-CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
-if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
-	set_config_var gpu_mem 128 /boot/config.txt
-fi
-sed /boot/config.txt -i -e "s/^startx/#startx/"
-sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
+
+echo "start_x=1" >> /boot/config.txt
+echo "gpu_mem=128" >> /boot/config.txt
 
 #enable SSH
 touch /boot/ssh
