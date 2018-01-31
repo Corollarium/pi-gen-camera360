2a4afff3ef7865fc4c14235b8adf7605417e388c
BrunoRB <brunorb8@hotmail.com>
2018-01-31 18:21:16 -0200

    up installing deb package

--- stage2/03-install-packages/00-run.sh
+++ stage2/03-install-packages/00-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 #install -m 774 -g pi -o pi $BINARY_PATH ${ROOTFS_DIR}/home/pi/camera360pi
-install -m 774 /home/build/camera360pi ${ROOTFS_DIR}/home/pi/camera360pi
+install -m 774 /home/build/deps/raspberry/camera360client.deb ${ROOTFS_DIR}/tmp/camera360client.deb
 install -m 774 files/camera360.service ${ROOTFS_DIR}/lib/systemd/system/
 
 on_chroot << EOF
+dpkg -i /tmp/camera360client.deb
 systemctl enable camera360
-chown pi.pi /home/pi/camera360pi
 EOF
