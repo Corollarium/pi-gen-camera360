cd24fad2d2247482cf0adf15a564035208b02483
BrunoRB <brunorb8@hotmail.com>
2018-08-27 17:32:11 -0300

    systemd init service cannot be marked as x

--- stage2/03-install-packages/00-run.sh
+++ stage2/03-install-packages/00-run.sh
@@ -2,7 +2,7 @@
 
 #install -m 774 -g pi -o pi $BINARY_PATH ${ROOTFS_DIR}/home/pi/camera360pi
 install -m 774 /home/build/deps/raspberry/camera360client.deb ${ROOTFS_DIR}/tmp/camera360client.deb
-install -m 774 files/camera360.service ${ROOTFS_DIR}/lib/systemd/system/
+install -m 664 files/camera360.service ${ROOTFS_DIR}/lib/systemd/system/
 
 on_chroot << EOF
 dpkg -i /tmp/camera360client.deb
